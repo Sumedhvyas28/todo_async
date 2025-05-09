2e8844313f9fd38afcbc47b9b54fc954c84f428b
Sumedhvyas28 <sumedhvyas10@gmail.com>
2025-05-09 19:35:13 +0530

    added ui pages and go router

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,1 +1,62 @@
 // navigation for go router page
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:todo_app/ui/add_task_screen.dart';
+import 'package:todo_app/ui/dashboard_screen.dart';
+import 'package:todo_app/ui/login_page.dart';
+import 'bloc/auth/auth_state.dart';
+import 'bloc/auth/auth_bloc.dart';
+
+class AppRouter {
+  final AuthBloc authBloc;
+
+  AppRouter({required this.authBloc});
+
+  late final GoRouter router = GoRouter(
+    refreshListenable: GoRouterRefreshStream(authBloc.stream),
+    redirect: (context, state) {
+      final isAuthenticated = authBloc.state.status == AuthStatus.authenticated;
+      final isLoginRoute = state.uri.toString() == '/login';
+
+      // If user is not authenticated and not on login route, redirect to login
+      if (!isAuthenticated && !isLoginRoute) {
+        return '/login';
+      }
+
+      // If user is authenticated and on login route, redirect to dashboard
+      if (isAuthenticated && isLoginRoute) {
+        return '/dashboard';
+      }
+
+      return null;
+    },
+    routes: [
+      GoRoute(path: '/', redirect: (_, __) => '/dashboard'),
+      GoRoute(path: '/login', builder: (context, state) => const LoginPage()),
+      GoRoute(
+        path: '/dashboard',
+        builder: (context, state) => const DashboardScreen(),
+      ),
+      GoRoute(path: '/add', builder: (context, state) => const AddTaskScreen()),
+    ],
+    errorBuilder: (context, state) =>
+        Scaffold(body: Center(child: Text('Error: ${state.error}'))),
+  );
+}
+
+// Helper class to make Bloc's state stream usable with GoRouter
+class GoRouterRefreshStream extends ChangeNotifier {
+  late final StreamSubscription<dynamic> _subscription;
+
+  GoRouterRefreshStream(Stream<dynamic> stream) {
+    _subscription = stream.listen((_) => notifyListeners());
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/app_router.dart';
+import 'package:todo_app/bloc/auth/auth_bloc.dart';
+import 'package:todo_app/data/auth_repository.dart';
 import 'package:todo_app/firebase_options.dart';
 import 'package:todo_app/ui/login_page.dart';
 
@@ -17,13 +21,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) =>
+                AuthBloc(authRepository: context.read<AuthRepository>()))
+      ],
+      child: Builder(
+        builder: (context) {
+          final appRouter = AppRouter(authBloc: context.read<AuthBloc>());
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            ),
+            home: LoginPage(),
+          );
+        },
       ),
-      home: LoginPage(),
     );
   }
 }
